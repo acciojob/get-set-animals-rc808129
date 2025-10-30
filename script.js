f8d043fac2565e944d98a9434b7dc00112eb0c79
Raj Chaurasiya <rc808129@gmail.com>
2025-10-30 17:17:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //complete this code
 class Animal {
 	constructor(species){
-		this.species = species;
+		this._species = species;
 	}
-	get species(){
-		return this.species;
+	get specie(){
+		return this._species;
 	}
 	makeSound(){
-		console.log(`The ${this.species} makes a sound`)
+		console.log(`The ${this._species} makes a sound`)
 	}
 }
 
